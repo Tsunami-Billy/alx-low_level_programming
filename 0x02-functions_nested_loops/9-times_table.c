18503ae8b00785ba571aaa88425f955c9f914732
root <root@DESKTOP-LOET47B>
2023-02-23 08:52:37 +0100

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * @i, @result: variable
+ */
+void times_table(void);
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int result = i * 9;
+
+		if (result > 9)
+		{
+			_putchar(result / 10 + '0');
+		}
+		_putchar(result % 10 + '0');
+		_putchar('\n');
+	}
+}
