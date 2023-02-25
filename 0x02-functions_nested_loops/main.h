f936d1e1f9772489ae91148216e60ecb3b2b0fe7
root <root@DESKTOP-LOET47B>
2023-02-25 01:45:23 +0100

    task 100

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+_table.ciifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,5 +12,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
